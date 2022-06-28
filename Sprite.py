1d746930aa12067d2c17ef6609ce78bcf46501ee
Verulean <65474222+Verulean@users.noreply.github.com>
2022-06-28 16:14:55 -0400

    Add documentation for Sprite.py

--- Sprite.py
+++ Sprite.py
@@ -1,3 +1,7 @@
+"""
+This module implements a Sprite object for storing position data for, and the
+content-based hash of, a single image/sprite.
+"""
 from dataclasses import dataclass
 from pathlib import Path
 from PIL import Image
@@ -6,6 +10,13 @@ from typing import Optional
 
 @dataclass
 class Sprite:
+    """
+    The Sprite class stores details of the position and orientation of a sprite
+    within a CustomKnight spritesheet. Sprite objects also track the state of
+    the underlying sprite image file, updating the sprite content and hash
+    when the file has been modified.
+    """
+
     identifier: int
     x: int
     y: int
@@ -19,6 +30,15 @@ class Sprite:
     mtime: Optional[float] = None
 
     def __update_file(self) -> None:
+        """
+        Checks if the source image has been modified, and recomputes the
+        cropped sprite content and resulting image hash if so.
+
+        Returns
+        -------
+        None.
+
+        """
         mtime: float = self.path.stat().st_mtime
         if mtime == self.mtime:
             return
@@ -33,14 +53,42 @@ class Sprite:
 
     @property
     def image_hash(self) -> int:
+        """
+        Getter for the image hash of the sprite content in the image file.
+
+        Returns
+        -------
+        int
+            The current hash.
+
+        """
         self.__update_file()
         return self.__hash
 
     @property
     def content(self) -> Image.Image:
+        """
+        Getter for the sprite content of the image file.
+
+        Returns
+        -------
+        Image
+            A PIL image with the underlying image file, cropped to the sprite
+            borders.
+
+        """
         self.__update_file()
         return self.__content
 
     @property
     def animation(self) -> str:
+        """
+        Returns the name of the animation the sprite is in.
+
+        Returns
+        -------
+        str
+            The animation name.
+
+        """
         return self.path.parent.name
